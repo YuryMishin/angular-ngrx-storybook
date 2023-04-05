a1ace10387d8f787b8c93fcc038daca85054a782
ymishin <yury.mishin96@gmail.com>
2023-04-05 07:15:35 +0300

    action story was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button.component';
+import { SimpleKnobsComponent } from './components/knobs.component';
+import { AllKnobsComponent } from './components/all-knobs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    SimpleKnobsComponent,
+    AllKnobsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
