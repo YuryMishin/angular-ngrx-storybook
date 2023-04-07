bb14f87ed0f9d4e928c52dbc785bb0f48bec53c8
ymishin <yury.mishin96@gmail.com>
2023-04-07 07:57:20 +0300

    notes and app stories

---
+++ src/stories/app.stories.ts
@@ -0,0 +1,25 @@
+import { storiesOf } from '@storybook/angular';
+import { withKnobs, text } from '@storybook/addon-knobs/angular';
+import { wTests } from '../../.storybook/withTests';
+import { action } from '@storybook/addon-actions';
+import { moduleMetadata } from '@storybook/angular';
+import { AppComponent } from '../app/app.component';
+
+storiesOf('My App', module)
+  .addDecorator(
+    moduleMetadata({
+      declarations: [AppComponent]
+    })
+  )
+  .add('simple app component', () => ({
+    template: `
+      <app-root
+        [title]="title"
+        (action)="onAction($event)">
+      </app-root>
+    `,
+    props: {
+      title: text('title', 'Hello World'),
+      onAction: action('Action happened')
+    }
+  }));
